f919d00c641bb885eb2e6a639d7e0314d792b925
nino mal <nino_mrt@hotmail.com>
2024-05-16 19:33:53 -0300

    Fixed up(main): add first carctVazio

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 def main():
-    carctCheio = "I"
-    carctVazio = "-"
+    CARCTCHEIO = "I"
+    CARCTVAZIO = "-"
     numeroCheio = 0
     numeroVazio = 0
     desenho = ""
@@ -8,43 +8,49 @@ def main():
     finura = 0
     quantidade = 0
     entrada1 = "continue"
-    nova = 0
     paceta = 0
-    novadesneho = 1
+    nova = 0
+    novaDesenho = 0
     finura = 0
-    
     
     finura = int(input("Finura: "))
     quantidade = int(input("Quantidade: "))
+    numeroVazio = int(input("numero Vazio: "))
+    for desenho2 in range(numeroVazio):
+                    desenho += CARCTVAZIO
     while paceta < quantidade:
         while(entrada1 != "n"):
-            numeroCheio = int(input("numero cheio"))
-            numeroVazio = int(input("numero Vazio"))
+            numeroCheio = int(input("numero cheio: "))
+            numeroVazio = int(input("numero Vazio: "))
             for desenho1 in range(numeroCheio):
-                    desenho += carctCheio
+                    desenho += CARCTCHEIO
             for desenho2 in range(numeroVazio):
-                    desenho += carctVazio
+                    desenho += CARCTVAZIO
             desenhoSoma += (numeroCheio + numeroVazio) 
-            entrada1 = input("Continue? ")  
-        desenhoSoma += numeroCheio + numeroVazio
+            entrada1 = input("Continue? digite n para sair: ")  
+            desenhoSoma += numeroCheio + numeroVazio
         for desenho1 in range(numeroCheio):
-                desenho += carctCheio
+                desenho += CARCTCHEIO
         for desenho2 in range(numeroVazio):
-                desenho += carctVazio
-        paceta+=1
-    
+                desenho += CARCTVAZIO
+        desenhoSoma += numeroCheio + numeroVazio
+        paceta+=1 
+
     finura = finura * 2
-    print("Paceta", novadesneho)
+    print("Paceta", novaDesenho)
     for teste in desenho:
         if nova >= finura :
             print(teste ,end="")
-            novadesneho+=1
+            novaDesenho +=1
             print()
-            print("Paceta", novadesneho )
+            print("Paceta", novaDesenho )
             nova = 0
         else:
             print(teste ,end="")
             nova += 1
+                        
+    
+    
             
             
              
@@ -53,8 +59,7 @@ def main():
     
     
     
-    
-    
+   
     
     
 if __name__ == "__main__":
